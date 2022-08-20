f125afd8e12ea51f4f859eeffbe4693f9c677e45
Jaival Patel <pateljaival05@gmail.com>
2022-08-19 21:49:16 -0400

    second button and css refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Button_one from './Button1'
+import Button_Two from './Button2';
 
 function App() {
+
   return (
-    <div className="App">
-      <h1> Hello </h1>
+    <div className="ArticleButtonList">
+      <ul> 
+        <Button_one />
+        <Button_Two />
+        <h3> Button 3 </h3>
+      </ul>
     </div>
   );
 }
