f8d4082a8e5379bde7913be98ed4cc971f69b53d
Jaival Patel <pateljaival05@gmail.com>
2022-08-19 21:21:33 -0400

    Buttonjs

---
+++ src/Button1.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function Button_One() {
+
+  return (
+    <div>
+      <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+    
+      <div className="Button_One">
+        <button type="button">Investing 💰</button>
+      </div>
+    </div>
+  );
+}
