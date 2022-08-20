425592017506d9b0b5c25c6a9cb197e70d18efc4
mariyatur27 <63456223+mariyatur27@users.noreply.github.com>
2022-08-20 11:01:20 -0400

    created js array of objects and the investment article pages

--- src/InvestingPage.js
+++ src/InvestingPage.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import {investment_articles} from './data/investment_articles'
 
 export default function Investing_Page() {
   return (
     <div class='coverpage-background'>
         <div className='centered-text'>Investing</div>
-        <div id='investing articles'></div>
+        <div id='investing-articles'>
+          {investment_articles.map((data, key) => {
+            return (
+              <a href=''>
+                <div className='article-box' key={key} id={data.id}>
+                  <div className='color-header'></div>
+                  <div className='article-title'>{data.article_name}</div>
+                  <div className='article-author'>By: {data.author}</div>
+                  <div className='article-dscr'>{data.sub_title}</div>
+                </div>
+              </a>
+            )
+          })}
+        </div>
     </div>
   )
 }
---
+++ src/data/investment_articles.js
@@ -0,0 +1,26 @@
+export const investment_articles = [
+    {
+        article_name: 'Stock Market Crashes',
+        author: 'Andrew Goldman',
+        sub_title: "Want to invest like a pro? Learn the basics of investing from us and we'll have you on the road to investing in no time.",
+        id:'invest-article-1'
+    },
+    {
+        article_name: 'Stock Market Crashes',
+        author: 'Andrew Goldman',
+        sub_title: "Want to invest like a pro? Learn the basics of investing from us and we'll have you on the road to investing in no time.",
+        id:'invest-article-1'
+    },
+    {
+        article_name: 'Stock Market Crashes',
+        author: 'Andrew Goldman',
+        sub_title: "Want to invest like a pro? Learn the basics of investing from us and we'll have you on the road to investing in no time.",
+        id:'invest-article-1'
+    },
+    {
+        article_name: 'Stock Market Crashes',
+        author: 'Andrew Goldman',
+        sub_title: "Want to invest like a pro? Learn the basics of investing from us and we'll have you on the road to investing in no time.",
+        id:'invest-article-1'
+    }
+]
